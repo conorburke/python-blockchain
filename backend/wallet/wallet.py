f2f5d1d3a0c90346241269785432d7ee17fa6007
conorburke <cjburke89@gmail.com>
2020-05-03 15:24:41 +0900

    set up wallets and transactions with tests

---
+++ backend/wallet/wallet.py
@@ -0,0 +1,57 @@
+import uuid
+import json
+
+from cryptography.exceptions import InvalidSignature
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import ec
+
+from backend.config import STARTING_BALANCE
+
+
+class Wallet:
+    """
+    An individual wallet for a miner.
+    Keeps track of miner's balance.
+    Allows miner to authorize transactions.
+    """
+
+    def __init__(self):
+        self.address = str(uuid.uuid4())[0:8]
+        self.balance = STARTING_BALANCE
+        self.private_key = ec.generate_private_key(ec.SECP256K1(), default_backend())
+        self.public_key = self.private_key.public_key()
+
+    def sign(self, data):
+        """
+        Generates a signatuare based on the data using the local private key
+        """
+
+        # data has to be bytes-like, so use json.dumps and then encode it
+        return self.private_key.sign(json.dumps(data).encode('utf-8'), ec.ECDSA(hashes.SHA256()))
+
+    @staticmethod
+    def verify(public_key, data, signature):
+        """
+        Verify a signature based on the public key and the data
+        """
+        try:
+            public_key.verify(signature, json.dumps(data).encode('utf-8'), ec.ECDSA(hashes.SHA256()))
+            return True
+        except InvalidSignature:
+            return False
+
+if __name__ == '__main__':
+    wallet = Wallet()
+    print(f'wallet: {wallet.__dict__}')
+
+    data = {'foo': 'bar'}
+
+    signature = wallet.sign(data)
+    print(f'signature: {signature}')
+
+    should_be_valid = Wallet.verify(wallet.public_key, data, signature)
+    print(f'should be valid: {should_be_valid}')
+
+    should_be_invalid = Wallet.verify(Wallet().public_key, data, signature)
+    print(f'should be invalid: {should_be_invalid}')
